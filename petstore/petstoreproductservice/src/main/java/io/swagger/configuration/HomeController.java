092e005889e64fc82ecadfbd5fef904ff304580d
Kir Tananushka <kir.tananushka@verifone.com>
2023-08-02 21:40:18 +0400

    Fix Swagger issue

--- petstore/petstoreproductservice/src/main/java/io/swagger/configuration/HomeController.java
+++ petstore/petstoreproductservice/src/main/java/io/swagger/configuration/HomeController.java
@@ -4,13 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * Home redirection to swagger api documentation 
+ * Home redirection to swagger api documentation
  */
 @Controller
 public class HomeController {
     @RequestMapping(value = "/")
     public String index() {
-        System.out.println("swagger-ui.html");
-        return "redirect:swagger-ui.html";
+        System.out.println("swagger-ui/");
+        return "redirect:swagger-ui/";
     }
 }
